f28e6fa022c7587513f5b60382ccf68596383412
Eric Dale <ericd517@outlook.com>
2022-10-23 12:12:01 -0300

    Conditional single class binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,42 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
   title: string = 'todo-list';
-  tasks: string[] = [
-    "Visit Brook",
-    "Call Brook",
-    "Take Brook on a date",
-    "Drive to Brook's house",
-    "Talk to Brook"
+  tasks: Task[] = [
+    new Task("Visit Brook"),
+    new Task("Call Brook"),
+    new Task("Take Brook on a date"),
+    new Task("Drive to Brook's house"),
+    new Task("Talk to Brook")
   ];
+
+  add(newTask: string): void{
+    this.tasks.push(new Task(newTask));
+  }   
+
+  remove(existingTask: Task): void{
+    let userConfirmed = confirm(`Hey, WAIT! Are you 100% sure you want to remove the following task? \n "${existingTask}"`);
+  
+    if(userConfirmed){
+      this.tasks = this.tasks.filter(task => task != existingTask);
+    }
+  }
+
+  markAsDone(task: Task){
+    task.isDone = true;
+  }
+
+}
+
+class Task {
+
+  public isDone: boolean = false;
+
+  constructor(public title: string){
+
+  }
+
 }
